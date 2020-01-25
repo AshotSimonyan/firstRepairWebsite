5cb851df858fe9e10d9a3b9f5bd56ab9b13f3ce9
Ashot Simonyan <a.simonyan.stdev@gmail.com>
2020-01-26 01:26:11 +0400

    first push

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.home-carousel').slick({
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+        autoplay: true
+    });
+});
+
